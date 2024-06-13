100afd400ecf48aaa15d3b8a269d2b4956c73211
Runxi Yu <me@runxiyu.org>
2024-06-13 08:00:00 +0800

    emacs: ivy and circe

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -3,7 +3,21 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(evil)))
+ '(package-selected-packages '(ivy circe evil)))
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(package-initialize)
+(setq circe-network-options
+      '(("rx"
+         :tls t
+	 :port 6697
+         :tls-keylist (("/home/runxiyu/.local/share/secrets/irc.pem"
+                        "/home/runxiyu/.local/share/secrets/irc.pem"))
+         :sasl-external t
+         :nick "runxiyu_circe"
+         :channels ("#chat")
+         )))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
