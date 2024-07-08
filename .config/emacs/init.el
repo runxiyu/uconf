a06b0b0a12e2f03180b9ad57349afb8dc74f9d17
Runxi Yu <me@runxiyu.org>
2024-07-08 08:00:00 +0800

    Emacs config still broken

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -1,10 +1,7 @@
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages '(ivy circe evil)))
 (require 'package)
+(use-package ivy :ensure t)
+(use-package circe :ensure t) 
+(use-package evil :ensure t)
 (add-to-list 'package-archives
              '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 (package-initialize)
@@ -18,6 +15,14 @@
          :nick "runxiyu_circe"
          :channels ("#chat")
          )))
+
+(setq inhibit-startup-message t)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(evil circe ivy)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
