c0212f5767ac08ad882bfada736f35f9f9686ebf
Runxi Yu <me@runxiyu.org>
2025-01-17 11:17:46 +0800

    Update

--- .local/sys/rxbar/rxbar.c
+++ .local/sys/rxbar/rxbar.c
@@ -23,8 +23,11 @@ int fdt, fds, fdc;
 time_t t1;
 char dmesg_buf[1024];
 
-void get_latest_dmesg(char *buffer, size_t size) {
-	FILE *fp = popen("dmesg | tail -1", "r");
+void get_latest_dmesg(char *buffer, size_t size)
+{
+	FILE *fp =
+	    popen("journalctl --lines=1 --no-pager | sed 's/^[^s]*s22537 //'",
+		  "r");
 	if (fp == NULL) {
 		perror("popen failed");
 		return;
@@ -34,27 +37,24 @@ void get_latest_dmesg(char *buffer, size_t size) {
 		pclose(fp);
 		return;
 	}
-	pclose(fp);
+	unsigned long end = strlen(buffer) - 1;
+	if (buffer[end] == '\n')
+		buffer[end] = 0;
 
-	char *message_start = strchr(buffer, ']');
-	if (message_start) {
-		message_start++;
-		while (*message_start == ' ') message_start++;
-		size_t message_len = strlen(message_start);
-		if (message_len > 0 && message_start[message_len - 1] == '\n') {
-			message_start[message_len - 1] = '\0';
-		}
-		memmove(buffer, message_start, message_len + 1);
-	}
+	pclose(fp);
 }
 
 int main()
 {
-	fdt = open("/sys/class/power_supply/macsmc-battery/time_to_empty_now", O_RDONLY);
+	fdt =
+	    open("/sys/class/power_supply/macsmc-battery/time_to_empty_now",
+		 O_RDONLY);
 	fdc = open("/sys/class/power_supply/macsmc-battery/capacity", O_RDONLY);
 	fds = open("/sys/class/power_supply/macsmc-battery/status", O_RDONLY);
 	if (fdc == -1 || fds == -1) {
-		fdt = open("/sys/class/power_supply/BAT0/time_to_empty_now", O_RDONLY);
+		fdt =
+		    open("/sys/class/power_supply/BAT0/time_to_empty_now",
+			 O_RDONLY);
 		fdc = open("/sys/class/power_supply/BAT0/capacity", O_RDONLY);
 		fds = open("/sys/class/power_supply/BAT0/status", O_RDONLY);
 	}
@@ -69,13 +69,10 @@ int main()
 				dmesg_buf,
 				td.tm_year + 1900,
 				td.tm_mon + 1,
-				td.tm_mday,
-				td.tm_hour,
-				td.tm_min,
-				td.tm_sec
-			);
+				td.tm_mday, td.tm_hour, td.tm_min, td.tm_sec);
 			t = (t + 1) * 1000000;
-			for (t1 = time(NULL) * 1000000; t1 < t; t1 = time(NULL) * 1000000)
+			for (t1 = time(NULL) * 1000000; t1 < t;
+			     t1 = time(NULL) * 1000000)
 				usleep(t - t1);
 		}
 	}
@@ -87,7 +84,7 @@ int main()
 		pread(fds, buf, 1, 0);
 		len2 = pread(fdt, buf2, MAXLEN, 0);
 		buf[len] = '\0';
-		buf2[len2-1] = '\0';
+		buf2[len2 - 1] = '\0';
 		get_latest_dmesg(dmesg_buf, sizeof(dmesg_buf));
 		dprintf(STDOUT_FILENO,
 			"%s | %s.%s %d-%02d-%02d %02d:%02d:%02d\n",
@@ -96,13 +93,10 @@ int main()
 			buf2,
 			td.tm_year + 1900,
 			td.tm_mon + 1,
-			td.tm_mday,
-			td.tm_hour,
-			td.tm_min,
-			td.tm_sec
-		);
+			td.tm_mday, td.tm_hour, td.tm_min, td.tm_sec);
 		t = (t + 1) * 1000000;
-		for (t1 = time(NULL) * 1000000; t1 < t; t1 = time(NULL) * 1000000)
+		for (t1 = time(NULL) * 1000000; t1 < t;
+		     t1 = time(NULL) * 1000000)
 			usleep(t - t1);
 	}
 }
