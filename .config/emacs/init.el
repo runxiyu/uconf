5dadb04cb7ab07cd7228c119af50ce1c5dcbe383
Runxi Yu <me@runxiyu.org>
2025-01-23 18:26:22 +0800

    Update

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -6,7 +6,7 @@
 ;; Basics
 (set-face-attribute 'default
                     nil
-                    :height 120
+                    :height 130
                     :family "monospace")
 (set-face-attribute 'fixed-pitch-serif
                     nil
@@ -14,7 +14,7 @@
                     :family "monospace")
 (set-face-attribute 'variable-pitch
                     nil
-                    :height 120
+                    ;; :height 120
                     :family "Inter")
 					; (add-to-list 'default-frame-alist '(undecorated . t))
 (global-unset-key (kbd "C-z"))
@@ -38,104 +38,21 @@
 (use-package auctex
   :ensure t)
 
+;; PDF
+(require 'pdf-tools)
+
 ;; Themes
 (setq custom-safe-themes t)
+(load-theme 'acme)
 ;; (load-theme 'wombat)
 ;; (load-theme 'wheatgrass)
 ;; (load-theme 'adwaita)
 
 ;; Evil
-;; (setq evil-undo-system 'undo-redo
-;;       evil-want-fine-undo t)
-;; (require 'evil)
-;; (evil-mode 1)
-
-;; Meow
-(defun meow-setup ()
-  (setq meow-cheatsheet-layout meow-cheatsheet-layout-qwerty)
-  (meow-motion-overwrite-define-key
-   '("j" . meow-next)
-   '("k" . meow-prev)
-   '("<escape>" . ignore))
-  (meow-leader-define-key
-   ;; SPC j/k will run the original command in MOTION state.
-   '("j" . "H-j")
-   '("k" . "H-k")
-   ;; Use SPC (0-9) for digit arguments.
-   '("1" . meow-digit-argument)
-   '("2" . meow-digit-argument)
-   '("3" . meow-digit-argument)
-   '("4" . meow-digit-argument)
-   '("5" . meow-digit-argument)
-   '("6" . meow-digit-argument)
-   '("7" . meow-digit-argument)
-   '("8" . meow-digit-argument)
-   '("9" . meow-digit-argument)
-   '("0" . meow-digit-argument)
-   '("/" . meow-keypad-describe-key)
-   '("?" . meow-cheatsheet))
-  (meow-normal-define-key
-   '("0" . meow-expand-0)
-   '("9" . meow-expand-9)
-   '("8" . meow-expand-8)
-   '("7" . meow-expand-7)
-   '("6" . meow-expand-6)
-   '("5" . meow-expand-5)
-   '("4" . meow-expand-4)
-   '("3" . meow-expand-3)
-   '("2" . meow-expand-2)
-   '("1" . meow-expand-1)
-   '("-" . negative-argument)
-   '(";" . meow-reverse)
-   '("," . meow-inner-of-thing)
-   '("." . meow-bounds-of-thing)
-   '("[" . meow-beginning-of-thing)
-   '("]" . meow-end-of-thing)
-   '("a" . meow-append)
-   '("A" . meow-open-below)
-   '("b" . meow-back-word)
-   '("B" . meow-back-symbol)
-   '("c" . meow-change)
-   '("d" . meow-delete)
-   '("D" . meow-backward-delete)
-   '("e" . meow-next-word)
-   '("E" . meow-next-symbol)
-   '("f" . meow-find)
-   '("g" . meow-cancel-selection)
-   '("G" . meow-grab)
-   '("h" . meow-left)
-   '("H" . meow-left-expand)
-   '("i" . meow-insert)
-   '("I" . meow-open-above)
-   '("j" . meow-next)
-   '("J" . meow-next-expand)
-   '("k" . meow-prev)
-   '("K" . meow-prev-expand)
-   '("l" . meow-right)
-   '("L" . meow-right-expand)
-   '("m" . meow-join)
-   '("n" . meow-search)
-   '("o" . meow-block)
-   '("O" . meow-to-block)
-   '("p" . meow-yank)
-   '("q" . meow-quit)
-   '("Q" . meow-goto-line)
-   '("r" . meow-replace)
-   '("R" . meow-swap-grab)
-   '("s" . meow-kill)
-   '("t" . meow-till)
-   '("u" . meow-undo)
-   '("U" . meow-undo-in-selection)
-   '("v" . meow-visit)
-   '("w" . meow-mark-word)
-   '("W" . meow-mark-symbol)
-   '("x" . meow-line)
-   '("X" . meow-goto-line)
-   '("y" . meow-save)
-   '("Y" . meow-sync-grab)
-   '("z" . meow-pop-selection)
-   '("'" . repeat)
-   '("<escape>" . ignore)))
+(setq evil-undo-system 'undo-redo
+      evil-want-fine-undo t)
+(require 'evil)
+(evil-mode 1)
 
 ;; Undo
 (require 'undo-tree)
@@ -225,7 +142,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(meow evil solarized-theme vterm auctex maxima vterm-toggle undo-tree fzf go darkroom sly magit ivy circe)))
+   '(pdf-tools plan9-theme acme-theme meow evil solarized-theme vterm auctex maxima vterm-toggle undo-tree fzf go darkroom sly magit ivy circe)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -233,3 +150,4 @@
  ;; If there is more than one, they won't work right.
  )
 
+(put 'dired-find-alternate-file 'disabled nil)
